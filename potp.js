b9ecbb3c5127d3ea7ff374882aef90530dfdb19b
Erik Rodhe <erikrodhe@gmail.com>
2014-11-24 00:31:27 +0100

    Trying some canvas and drawing

--- potp.js
+++ potp.js
@@ -1,28 +1,80 @@
 // Game implementation
+            
+
 
 var Target = function(targetValue) {
+
+
     this.targetValue = targetValue;
     this.currentValue = 0;
     this.includedParts = [];
+    this.posx = 250;
+    this.posy = 250;
+    this.radius = 100;
 
     this.includePart = function(part) {
-	this.currentValue += part.value;
-	this.includedParts.push(part);
+        this.currentValue += part.value;
+        this.includedParts.push(part);
+    };
+
+    this.draw = function() {
+        var b = document.getElementById("myCanvas");
+        var ctx = b.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.posx, this.posy, this.radius,0,2*Math.PI);
+        ctx.stroke();
+        ctx.font="40px Georgia";
+        ctx.fillText(this.targetValue, this.posx - 30, this.posy - 10);
+        ctx.font="40px Georgia";
+        ctx.fillText(this.currentValue, this.posx - 30, this.posy + 20);
     };
 };
 
 var Part = function(value) {
-    this.value = value
+    this.value = value;
+    this.posx = 50+ Math.random()*300;
+    this.posy = 100;
+    this.radius = 30;
+
+    this.draw = function() {
+        var b = document.getElementById("myCanvas");
+        var ctx = b.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(this.posx, this.posy, this.radius,0,2*Math.PI);
+        ctx.stroke();
+        ctx.font="40px Georgia";
+        ctx.fillText(this.value, this.posx - 20, this.posy + 10);
+    };
 };
 
 var GameSession = function() {
     this.target = new Target(100);
     this.parts = [];
 
-    this.split = function() {
-	var part1 = new Part(50);
-	var part2 = new Part(50);
-	this.parts.push(part1);
-	this.parts.push(part2);
+    this.newGame = function () {
+        this.target.draw();
     };
+
+    this.split = function() {
+        var part1 = new Part(50);
+        var part2 = new Part(50);
+        this.parts.push(part1);
+        this.parts.push(part2);
+
+        for (i = 0; i < this.parts.length; i++) { 
+            this.parts[i].draw();
+        };
+
+    };
+
+
 };
+
+var gamesession = document.getElementById("body").onload = function(){
+    var gamesession = new GameSession();
+    gamesession.newGame();
+    gamesession.split();
+};
+
+
+
--- spec/TargetSpec.js
+++ spec/TargetSpec.js
@@ -1,41 +1,41 @@
 describe("Target", function() {
     describe("on Initialization", function() {
-	var target;
-	beforeEach(function() {
-      	    target = new Target(100);
-      	});
+        var target;
+        beforeEach(function() {
+            target = new Target(100);
+        });
 
-      	it("contains the provided TargetValue", function() {
-	    expect(target.targetValue).toBe(100); 	
-	}); 
+        it("contains the provided TargetValue", function() {
+            expect(target.targetValue).toBe(100); 	
+        }); 
 
-	it("contains the default currentValue 0", function() {
-	    expect(target.currentValue).toBe(0);
-	});
-
-
-
+        it("contains the default currentValue 0", function() {
+            expect(target.currentValue).toBe(0);
+        });
     });
 
     describe("includes parts", function() {
 
-	it("updates currentValue", function() {
-	    var target = new Target(100);
-	    currentValue = target.currentValue;
-	    var part   = new Part(50);
-	    target.includePart(part);
-	    expect(target.currentValue).toBe(currentValue+part.value);
-	});
+        it("updates currentValue", function() {
+            var target = new Target(100);
+            var part   = new Part(50);
+            currentValue = target.currentValue;
+            target.includePart(part);
+                
+            expect(target.currentValue).toBe(currentValue+part.value);
+        });
 
-	it("keeps track of included parts", function() {
-	    var target  = new Target(100);
-	    var part1   = new Part(50);
-	    var part2   = new Part(25);
-	    target.includePart(part1);
-	    target.includePart(part2);
+        it("keeps track of included parts", function() {
+            var target  = new Target(100);
+            var part1   = new Part(50);
+            var part2   = new Part(25);
+            target.includePart(part1);
+            target.includePart(part2);
 
-	    expect(target.includedParts).toContain(part1);
-	    expect(target.includedParts).toContain(part2);
-	});
+            expect(target.includedParts).toContain(part1);
+            expect(target.includedParts).toContain(part2);
+        });
+
     }); 
 });
+
