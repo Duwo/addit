f76e2ab4a55a61a65e660d9190abc1f45aab20cc
Karl Johansson <karljoh@sics.se>
2014-11-23 17:35:10 +0100

    Allow inclusion of parts in targets

--- potp.js
+++ potp.js
@@ -1,12 +1,16 @@
 // Game implementation
-<<<<<<< HEAD
+
 var Target = function(targetValue) {
-  this.targetValue = targetValue;
-  this.currentValue = 0;
+    this.targetValue = targetValue;
+    this.currentValue = 0;
+    this.includedParts = [];
+
+    this.includePart = function(part) {
+	this.currentValue += part.value;
+	this.includedParts.push(part);
+    }
 }
-=======
 
 var Part = function(value) {
     this.value = value
 }
->>>>>>> 9c1a96df6cff81695649f9cd81b142a31caa1605
--- spec/TargetSpec.js
+++ spec/TargetSpec.js
@@ -1,29 +1,41 @@
 describe("Target", function() {
-	describe("on Initialization", function(){
-		var target;
-		beforeEach(function() {
-      		target = new Target(100);
+    describe("on Initialization", function() {
+	var target;
+	beforeEach(function() {
+      	    target = new Target(100);
       	});
 
       	it("contains the provided TargetValue", function() {
-	  		expect(target.targetValue).toBe(100); 	
-		}); 
+	    expect(target.targetValue).toBe(100); 	
+	}); 
 
-		it("contains the default currentValue 0", function() {
-			expect(target.currentValue).toBe(0);
-		});
-
-
-
+	it("contains the default currentValue 0", function() {
+	    expect(target.currentValue).toBe(0);
 	});
 
-	describe("includes a parts", function(){
-		it("updates currentValue", function(){
-			var target = new Target(100);
-			currentValue = target.currentValue;
-			var part   = new Part(50);
-			target.includePart(part);
-			expect(target.currentValue).toBe(currentValue+part.value);
-		});
-	}); 
+
+
+    });
+
+    describe("includes parts", function() {
+
+	it("updates currentValue", function() {
+	    var target = new Target(100);
+	    currentValue = target.currentValue;
+	    var part   = new Part(50);
+	    target.includePart(part);
+	    expect(target.currentValue).toBe(currentValue+part.value);
+	});
+
+	it("keeps track of included parts", function() {
+	    var target  = new Target(100);
+	    var part1   = new Part(50);
+	    var part2   = new Part(25);
+	    target.includePart(part1);
+	    target.includePart(part2);
+
+	    expect(target.includedParts).toContain(part1);
+	    expect(target.includedParts).toContain(part2);
+	});
+    }); 
 });
