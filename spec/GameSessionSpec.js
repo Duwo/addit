4feec2a8ad922c58a86c3549f3c1d4d985ef2e31
Erik Rodhe <erikrodhe@gmail.com>
2014-11-25 23:39:28 +0100

    Add 50s to target

--- spec/GameSessionSpec.js
+++ spec/GameSessionSpec.js
@@ -2,24 +2,24 @@ describe("GameSession", function() {
     
     describe("on initialization", function() {
 	
-	it("should have a target", function() {
-	    var session = new GameSession();
-	    expect(session.target).toBeDefined();
-	});
+	   it("should have a target", function() {
+	       var session = new GameSession();
+	       expect(session.target).toBeDefined();
+	   });
 
-	it("should not have any parts", function() {
-	    var session = new GameSession();
-	    expect(session.parts).toEqual([]);
-	});
+	   it("should not have any parts", function() {
+	       var session = new GameSession();
+	       expect(session.parts).toEqual([]);
+	   });
     });
 
     describe("when splitting target", function() {
 	
-	it("should create parts", function() {
-	    var session = new GameSession();
-	    session.split();
-	    expect(session.parts).not.toEqual([]);
-	});
+	   it("should create parts", function() {
+	       var session = new GameSession();
+	       session.setup();
+	       expect(session.parts).not.toEqual([]);
+	   });
     });
 });
 
