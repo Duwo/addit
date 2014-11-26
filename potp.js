226bf1c9b6e13cc21ba1345240d54001fd5e5267
Erik Rodhe <erikrodhe@gmail.com>
2014-11-26 08:11:20 +0100

    reverted files

--- potp.js
+++ potp.js
@@ -1,271 +1,28 @@
 // Game implementation
-            
-
 
 var Target = function(targetValue) {
-
-
     this.targetValue = targetValue;
     this.currentValue = 0;
     this.includedParts = [];
-    this.posx = 250;
-    this.posy = 250;
-    this.radius = 100;
-    this.partIds =[];
 
     this.includePart = function(part) {
-        this.currentValue += part.value;
-        this.includedParts.push(part);
-        this.partIds.push(part.id);
-        console.log(this.includedParts)
-        console.log(this.partIds)
-    };
-
-    this.excludePart = function(part) {
-        this.currentValue -= part.value;
-        var index = this.partIds.indexOf(part.id);
-        if (index > -1) {
-            this.partIds.splice(index, 1);
-            this.includedParts.splice(index, 1);
-
-        }
-        console.log(this.includedParts)
-        console.log(this.partIds)
-    };
-
-    this.draw = function() {
-        context.beginPath();
-        context.arc(this.posx, this.posy, this.radius,0,2*Math.PI);
-        context.stroke();
-        context.font="40px Georgia";
-        context.fillText(this.targetValue, this.posx - 30, this.posy - 10);
-        context.font="40px Georgia";
-        context.fillText(this.currentValue, this.posx - 30, this.posy + 20);
+	this.currentValue += part.value;
+	this.includedParts.push(part);
     };
 };
-
-var idCounter = 0;
 
 var Part = function(value) {
-    this.value = value;
-    this.posx = 50+ Math.random()*300;
-    this.posy = 100;
-    this.radius = 30;
-    this.id = idCounter++;
-    console.log(idCounter)
-
-
-    this.draw = function() {
-
-        context.beginPath();
-        context.arc(this.posx, this.posy, this.radius,0,2*Math.PI);
-        context.stroke();
-        context.font="40px Georgia";
-        context.fillText(this.value, this.posx - 20, this.posy + 10);
-    };
-
-    function hitTest(mx,my) {
-        
-        var dx;
-        var dy;
-        dx = mx - this.posx;
-        dy = my - this.posy;
-        
-        //a "hit" will be registered if the distance away from the center is less than the radius of the circular object        
-        return (dx*dx + dy*dy < this.raius*this.radius);
-    }
-
+    this.value = value
 };
-
-
-
-
 
 var GameSession = function() {
+    this.target = new Target(100);
+    this.parts = [];
 
-    var theCanvas = document.getElementById("myCanvas");
-    var bRect = theCanvas.getBoundingClientRect();
-
-    var number_of_parts;
-    var parts;
-    var target;
-    var context = theCanvas.getContext("2d");
-    var dragIndex;
-    var dragHoldX;
-    var dragHoldY;
-
-    this.init = function() {
-
-        //later in make target function
-        target = new Target(100);
-        parts = [];
-
-        // Later in make parts function
-        var part1 = new Part(50);
-        var part2 = new Part(50);
-
-        parts.push(part1);
-        parts.push(part2);
-
-        target.draw(); 
- 
+    this.split = function() {
+	var part1 = new Part(50);
+	var part2 = new Part(50);
+	this.parts.push(part1);
+	this.parts.push(part2);
     };
-    function drawParts() {
-        for (i = 0; i < parts.length; i++) { 
-            parts[i].draw();
-        };
-    };
-
-
-    this.newGame = function() { 
-        this.reset();
-        target.draw();
-        drawParts();
-
-        theCanvas.addEventListener("mousedown", mouseDownListener, false);
-    };
-
-    function mouseDownListener(evt) {
-        var i;
-        //We are going to pay attention to the layering order of the objects so that if a mouse down occurs over more than object,
-        //only the topmost one will be dragged. 
-
-        // THIS NEEDS TO CHANGE
-        var highestIndex = -1;
-
-
-        mouseX = (evt.clientX - bRect.left)*(theCanvas.width/bRect.width);
-        mouseY = (evt.clientY - bRect.top)*(theCanvas.height/bRect.height);
-
-        for (i=0; i < parts.length; i++) {
-            if  (hitTest(parts[i], mouseX, mouseY)) {
-                dragging = true;
-                if (i > highestIndex) {
-                    //We will pay attention to the point on the object where the mouse is "holding" the object:
-                    dragHoldX = mouseX - parts[i].posx;
-                    dragHoldY = mouseY - parts[i].posy;
-                    highestIndex = i;
-                    dragIndex = i;
-                }
-            }
-        }
-        
-        if (dragging) {
-            window.addEventListener("mousemove", mouseMoveListener, false);
-        }
-
-        theCanvas.removeEventListener("mousedown", mouseDownListener, false);
-        window.addEventListener("mouseup", mouseUpListener, false);
-    }
-
-    function mouseUpListener(evt) {
-        mouseX = (evt.clientX - bRect.left)*(theCanvas.width/bRect.width);
-        mouseY = (evt.clientY - bRect.top)*(theCanvas.height/bRect.height);
-
-        theCanvas.addEventListener("mousedown", mouseDownListener, false);
-        window.removeEventListener("mouseup", mouseUpListener, false);
-
-        if (hitTest(target, mouseX, mouseY) && (target.partIds.indexOf(parts[dragIndex].id) < 0)) {
-            target.includePart(parts[dragIndex])
-            context.clearRect(0, 0, b.width, b.height);
-            context.restore();
-            target.draw();
-            drawParts();
-
-        } else if ( dragging === true && hitTest(target, mouseX,mouseY) === false && (target.partIds.indexOf(parts[dragIndex].id) > -1)) {
-            target.excludePart(parts[dragIndex])
-            context.clearRect(0, 0, b.width, b.height);
-            context.restore();
-            target.draw();
-            drawParts();
-        };
-        
-        if (dragging) {
-            dragging = false;
-            window.removeEventListener("mousemove", mouseMoveListener, false);
-        };
-
-
-    }    
-
-    function hitTest(shape,mx,my) {
-        
-        var dx;
-        var dy;
-        dx = mx - shape.posx;
-        dy = my - shape.posy;
-        
-        //a "hit" will be registered if the distance away from the center is less than the radius of the circular object        
-        return (dx*dx + dy*dy < shape.radius*shape.radius);
-    };
-
-    function mouseMoveListener(evt) {
-        var posX;
-        var posY;
-        var partRad = parts[dragIndex].radius;
-        var minX = partRad;
-        var maxX = theCanvas.width - partRad;
-        var minY = partRad;
-        var maxY = theCanvas.height - partRad;
-        //getting mouse position correctly 
-        var bRect = theCanvas.getBoundingClientRect();
-        mouseX = (evt.clientX - bRect.left)*(theCanvas.width/bRect.width);
-        mouseY = (evt.clientY - bRect.top)*(theCanvas.height/bRect.height);
-            
-        //clamp x and y positions to prevent object from dragging outside of canvas
-        posX = mouseX - dragHoldX;
-        posX = (posX < minX) ? minX : ((posX > maxX) ? maxX : posX);
-        posY = mouseY - dragHoldY;
-        posY = (posY < minY) ? minY : ((posY > maxY) ? maxY : posY);
-            
-        parts[dragIndex].posx = posX;
-        parts[dragIndex].posy = posY;
-
-        context.clearRect(0, 0, b.width, b.height);
-        context.restore();
-        target.draw();
-        drawParts();
-
-    }; 
-    
-    this.update = function() {
-        // Draw all parts
-
-        // Check if click is inside target and recalculate
-    
-        // Check if target is completed
-
-    };
-
-
-
-    this.reset = function() {
-        this.init();
-        context.clearRect(0, 0, b.width, b.height);
-        context.restore();
-        
-        this.parts = [];
-    };
-
-
-
-    this.complete = function() {
-
-
-    };
-
-
 };
-var b = document.getElementById("myCanvas");
-var context = b.getContext("2d");
-
-gamesession = new GameSession()
-
-
-
-document.getElementById("body").onload = function() {gamesession.init()};
-
-
-
-
-
